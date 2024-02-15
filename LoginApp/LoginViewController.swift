fef3a7476b94d83f006a421187dc2f870f10d90f
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-02-15 23:16:49 +0700

    Initial Commit

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Alexey Efimov on 19/06/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    private let user = "User"
+    private let password = "Password"
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        welcomeVC?.userName = user
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password") {
+                    self.passwordTextField.text = ""
+                }
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(withTitle: "Oops!", andMessage: "Your name is \(user) 😉")
+            : showAlert(withTitle: "Oops!", andMessage: "Your password is \(password) 😉")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String, completion: (() -> Void)? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            completion?()
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
